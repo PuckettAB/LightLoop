11e467009ac4f7e26c7f833b04615772565e086d
PuckettAB <79866000+PuckettAB@users.noreply.github.com>
2022-01-11 13:17:44 -0500

    Create pulseDense.py

---
+++ pulseDense.py
@@ -0,0 +1,16 @@
+#4 emitters, each 90 degrees apart
+#12 used receivers
+class tissueDense1:
+	#the class is defined by which receiver is receiving the information
+	def __init__(self, pulseNum, receiverNum, rawData, calcDensity, angle, coordinates, hounsConv):
+		self.pulseNum = pulseNum		#each object is named by its pulseNum
+		self.receiverNum = receiverNum
+		self.rawData = rawData
+		self.calcDensity = calcDensity
+		self.angle = angle
+		self.coordinates = coordinates
+		self.hounsConv = hounsConv
+
+#pulseCount = counts how many pulses have occured
+pulse1 = tissueDense1(pulseCount,receiverNum,rawData, )
+def
