a8af7764793763cfc709f852af9a7762622ac4ed
PuckettAB <79866000+PuckettAB@users.noreply.github.com>
2022-02-18 13:16:33 -0500

    Update PulseLaser.py

--- PulseLaser.py
+++ PulseLaser.py
@@ -1,16 +1,14 @@
 #https://forums.raspberrypi.com/viewtopic.php?t=143484
 #Pulse light: https://raspberrypihq.com/making-a-led-blink-using-the-raspberry-pi-and-python/
 
-import RPi.GPIO as GPIO    # Import Raspberry Pi GPIO library
-from time import sleep     # Import the sleep function from the time module
-GPIO.setwarnings(False)    # Ignore warning for now
-GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
-GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)   # Set pin 8 to be an output pin and set initial value to low (off)
+from gpiozero import LED
+led = LED(2) #5 volt output
 
-while True: # Run forever
-    GPIO.output(8, GPIO.HIGH) # Turn on
-    sleep(.01)                  # Sleep for .01 second
-    GPIO.output(8, GPIO.LOW)  # Turn off
-    sleep(.01)                  # Sleep for .01 second
 
-    
+for x in range(2,6):
+    led.on()
+    sleep(.01)
+    led.off()
+    sleep(.01)
+#use 5 volts for laser
+#use ground for laser
